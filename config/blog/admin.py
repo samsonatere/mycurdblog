3d83fdffeb5eae7f36380178b81e5e443dce9626
samsonatere <samsonatere24@gmail.com.com>
2021-05-06 20:04:26 +0100

    well design

--- config/blog/admin.py
+++ config/blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+class CommentInline(admin.StackedInline):
+    model = Comment
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
--- config/blog/models.py
+++ config/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +13,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments',)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('post_detail')
